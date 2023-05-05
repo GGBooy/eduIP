2617eaa06b091bd973a15b2035d9ca152a5a3959
gjl <1079603003@qq.com>
2023-05-05 15:40:47 +0800

    获取ip

---
+++ format.py
@@ -0,0 +1,47 @@
+## get the web content from https://ispip.clang.cn/cernet_cidr.html
+
+import requests
+import json
+
+
+def getContent(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    response.encoding = 'utf-8'
+    json_response = response.content.decode()
+    return json_response
+
+
+# regular expression to match ip address, e.g. 127.0.0.1/32, return a list
+def getIpCidr(content):
+    import re
+    pattern = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d+')
+    ipCidr = pattern.findall(content)
+    return ipCidr
+
+# use the IP list to create a yaml file with the format like:
+# rules:
+#   - IP-CIDR,202.114.0.250/16,HUST,no-resolve
+#   - IP-CIDR,222.20.96.0/24,HUST,no-resolve
+
+def createYaml(ipCidr):
+    ip = ['IP-CIDR,'+item+',HUST,no-resolve' for item in ipCidr]
+    import yaml
+    with open('ip.yaml', 'w') as f:
+        yaml.dump({'rules': ip}, f)
+
+
+
+
+if __name__ == '__main__':
+    url = 'https://ispip.clang.cn/cernet_cidr.html'
+    content = getContent(url)
+    ipCidr = getIpCidr(content)
+    print(ipCidr)
+    createYaml(ipCidr)
+
+
+
